9f43014435e1bd18cd50eb449d47bda5804c711d
Breno Lira <lira_breno@hotmail.com>
2022-07-22 11:05:04 -0300

    chore: data + kickstart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,15 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import { AppProvider } from '../components/AppContext';
 
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider>
+    <>
       <Component {...pageProps} />
-    </AppProvider>
+    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,19 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
 import { FaGithub } from 'react-icons/fa';
-import { useTranslation } from 'react-i18next';
-
-import { AppContext } from '../components/AppContext';
 
 export default function Home() {
-  const appCtx = React.useContext(AppContext);
   const router = useRouter();
-  const { t, i18n } = useTranslation();
 
   return (
     <>
       <NextSeo
-        title="next-tailwind-boilerplate"
+        title="TBB - Sorting"
         description="This is description"
         openGraph={{
-          url: 'https://next-tailwind-boilerplate.haneyume.vercel.app/',
-          title: 'next-tailwind-boilerplate',
-          description: 'next-tailwind-boilerplate',
+          url: 'https://tbb-challenge.vercel.app/',
+          title: 'TBB - Sorting',
+          description: 'A React challenge about sorting data',
           images: [
             {
               url: '/aaa',
@@ -28,29 +23,15 @@ export default function Home() {
               alt: 'bg',
             },
           ],
-          site_name: 'next-tailwind-boilerplate',
+          site_name: 'TBB - Sorting',
         }}
       />
 
       <div className="border-b border-gray-300 mb-5">
         <div className="container mx-auto px-5">
           <div className="flex justify-start items-center py-5 space-x-3">
-            <div className="text-2xl text-gray-700">
-              next-tailwind-boilerplate
-            </div>
-
-            <div className="px-3 text-xs text-gray-400 border border-gray-400 rounded-md">
-              {t('light')}
-            </div>
-
+            <div className="text-2xl text-gray-700">The Brooklyn Brothers</div>
             <div className="flex-1" />
-
-            <select onChange={(evt) => i18n.changeLanguage(evt.target.value)}>
-              <option value="en">English</option>
-              <option value="ja">日本語</option>
-              <option value="zhHant">繁體中文</option>
-              <option value="zhHans">简体中文</option>
-            </select>
 
             <div
               className="cursor-pointer"
