5ba9645e0324adfe9d018f35076430b1c59fd5fc
Breno Lira <lira_breno@hotmail.com>
2022-07-22 13:16:00 -0300

    chore[data]: new data + new category

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, useMemo, useState } from 'react';
 import Image from 'next/image';
 import { NextSeo } from 'next-seo';
 import { FaGithub } from 'react-icons/fa';
-import data from '../data/data.json';
+import data from '../data/productsCategory.json';
 
 interface DataProps {
   name: string;
@@ -10,15 +10,17 @@ interface DataProps {
   id: string;
   images: {
     alt: string;
-    src: string;
+    asset: {
+      url: string;
+    };
   }[];
   category: {
+    _id: string;
     name: string;
-    id: string;
   };
 }
 
-type CategoryType = 'all' | 'rollon' | 'aerosol' | 'stick';
+type CategoryType = 'all' | 'alcoholengel' | 'aerosol' | 'barra' | 'jabónlíquido' | 'jabónbarra' | 'alcoholenspray' | 'alcoholenaerosol' | 'talco';
 
 export default function Home() {
   const [category, setCategory] = useState<CategoryType>('all');
@@ -36,11 +38,9 @@ export default function Home() {
           return <></>;
 
         return (
-          <div key={i} className="px-5 py-5 rounded-md bg-gray-100">
-            {/* API returns 404 - image not found with this src */}
-            {/* <Image src={item.images[0].src} alt={item.images[0].alt} height={50} width={50}/> */}
+          <div key={i} className="px-5 py-2 rounded-md bg-gray-100 flex gap-2 items-center justify-between">
+            <Image src={item.images[0].asset.url} alt={item.images[0].alt} height={50} width={50}/>
             <small>{item.name}</small>
-            <div className="text-sm text-gray-500"></div>
           </div>
         );
       }),
@@ -65,11 +65,11 @@ export default function Home() {
             },
           ],
           site_name: 'TBB - Sorting',
-          locale: 'en'
+          locale: 'en',
         }}
       />
 
-      <div className="border-b border-gray-300 mb-5 shadow-md">
+      <div className="border-b border-gray-300 mb-5 shadow-md sticky top-0 z-10 bg-gray-300">
         <div className="container mx-auto px-5">
           <div className="flex justify-between items-center py-5 space-x-3">
             <div className="text-2xl text-gray-700 md:block hidden">
@@ -84,8 +84,13 @@ export default function Home() {
             >
               <option value="all">All</option>
               <option value="aerosol">Aerosol</option>
-              <option value="rollon">Roll On</option>
-              <option value="stick">Stick</option>
+              <option value="alcoholengel">Alcool em Gel</option>
+              <option value="barra">Barra</option>
+              <option value="jabónlíquido">Sabão Líquido</option>
+              <option value="jabónbarra">Sabão Barra</option>
+              <option value="alcoholenspray">Alcool em Spray</option>
+              <option value="alcoholenaerosol">Alcool em Aerosol</option>
+              <option value="talco">Talco</option>
             </select>
 
             <div
@@ -102,7 +107,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="container mx-auto px-5">
+      <div className="container mx-auto px-5 pb-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
           {memoizedMap}
         </div>
